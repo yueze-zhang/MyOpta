309422af06259e2cf79143b536343eafd447dcf9
GaRiboy <734762673@qq.com>
2018-11-25 23:28:52 +0800

    3

--- src/main/java/org/optaplanner/examples/cloudbalancing/optional/score/CloudBalancingEasyScoreCalculator.java
+++ src/main/java/org/optaplanner/examples/cloudbalancing/optional/score/CloudBalancingEasyScoreCalculator.java
@@ -26,21 +26,25 @@ public class CloudBalancingEasyScoreCalculator implements EasyScoreCalculator<Cl
 
     /**
      * A very simple implementation. The double loop can easily be removed by using Maps as shown in
+     * 一个非常简单的实现。使用Maps可以很容易地删除双循环，如图所示
      * {@link CloudBalancingMapBasedEasyScoreCalculator#calculateScore(CloudBalance)}.
      */
+
     @Override
     public HardSoftScore calculateScore(CloudBalance cloudBalance) {
         int hardScore = 0;
         int softScore = 0;
+        //从可用计算机列表中选择一个计算机
         for (CloudComputer computer : cloudBalance.getComputerList()) {
             int cpuPowerUsage = 0;
             int memoryUsage = 0;
             int networkBandwidthUsage = 0;
             boolean used = false;
 
-            // Calculate usage
+            //从进程列表中选择一个进程
             for (CloudProcess process : cloudBalance.getProcessList()) {
                 if (computer.equals(process.getComputer())) {
+                    //如果当前选定的计算机有分配的任务，则进行以下内容
                     cpuPowerUsage += process.getRequiredCpuPower();
                     memoryUsage += process.getRequiredMemory();
                     networkBandwidthUsage += process.getRequiredNetworkBandwidth();
@@ -48,15 +52,17 @@ public class CloudBalancingEasyScoreCalculator implements EasyScoreCalculator<Cl
                 }
             }
 
-            // Hard constraints
-            int cpuPowerAvailable = computer.getCpuPower() - cpuPowerUsage;
+            // Hard constraints 硬约束
+            int cpuPowerAvailable = computer.getCpuPower() - cpuPowerUsage; //CPUPower可用资源 = 总共的资源 - 使用中的资源
             if (cpuPowerAvailable < 0) {
-                hardScore += cpuPowerAvailable;
+                hardScore += cpuPowerAvailable; //硬错误增加
             }
+            //同上检测内存可用
             int memoryAvailable = computer.getMemory() - memoryUsage;
             if (memoryAvailable < 0) {
                 hardScore += memoryAvailable;
             }
+            //同上检测带宽可用
             int networkBandwidthAvailable = computer.getNetworkBandwidth() - networkBandwidthUsage;
             if (networkBandwidthAvailable < 0) {
                 hardScore += networkBandwidthAvailable;
